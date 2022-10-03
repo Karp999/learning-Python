dc091799f2b9cd6c94cdd26b9e3b3723227372ac
Anna.K <kkk.aanya1996@mail.ru>
2022-10-03 19:52:56 +0300

    z5+5.jpg

--- Homework/hw2/z5.py
+++ Homework/hw2/z5.py
@@ -1,1 +1,13 @@
 # Задача 5. Реализуйте алгоритм перемешивания списка.
+
+import random
+print()
+
+initialList = [1,2,3,4,5,6,7,8,9]
+print(initialList)
+for i in range(len(initialList)):
+    k = initialList[i]
+    position = random.randint(0,8)
+    initialList[i] = initialList[position]
+    initialList[position] = k
+print(initialList)
