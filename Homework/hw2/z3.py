4d1f9eda38181ca9826b898c54e8d3638b60e78c
Anna.K <kkk.aanya1996@mail.ru>
2022-10-03 19:02:07 +0300

    z3 + 3.jpg

--- Homework/hw2/z3.py
+++ Homework/hw2/z3.py
@@ -2,3 +2,26 @@
 # и выведите на экран их сумму (округляйте до 3 знаков после запятой).    
 # Пример:    
 # Для n = 6: [2, 2.25, 2.37, 2.441, 2.488, 2.522]
+
+print()
+userN = int(input("Введите количество позиций для создания списка последовательности (1+1/n)^n: "))
+print()
+
+commonList = []
+def Sequence(userN):
+    for i in range(1,userN+1):
+        commonList.append(round((1+1/i)**i,3))
+    return(commonList)
+print ("Вывод последовательности: ", Sequence(userN))
+print()
+
+sum = 0
+for i in range(userN):
+    sum = sum + commonList[i]
+print("Сумма чисел последовательности (1+1/n)^n равна:", sum)
+print()
+
+
+
+
+
