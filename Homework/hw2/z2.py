8e95a3f0389398c73cc60fad2b867418706dacdd
Anna.K <kkk.aanya1996@mail.ru>
2022-10-03 18:31:43 +0300

    corrected z2 + 2.jpg

--- Homework/hw2/z2.py
+++ Homework/hw2/z2.py
@@ -2,17 +2,19 @@
 # Пример:
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-from re import I
-
-
 print()
 userN = int(input("Введите число N: "))
 
-def Factorial(userN): # решила сделать через факториал
+def Factorial(userN): 
+    commonList = [] # в общий список будет сохранятся набор произведений
     factorialUserN = 1 # первоначальное значение
-    for i in userN:
-        factorialUserN *= int(i) 
-        return(i)
+    for i in range(userN):
+        factorialUserN = factorialUserN*(i+1)
+        commonList.append(factorialUserN)
+    return(commonList)
 
 print("Произведение чисел от 1 до N равняется:", Factorial(userN))
+print()
+
+
 
