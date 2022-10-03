97fcd60f52e4541eb28c1717167fbfe8ac50d9c5
Anna.K <kkk.aanya1996@mail.ru>
2022-10-03 19:31:18 +0300

    z4

--- Homework/hw2/z4.py
+++ Homework/hw2/z4.py
@@ -1,3 +1,32 @@
 # Задача 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 # Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
+
+from random import randint
+
+def Multiplication(userNumber):
+    commonList = []
+    multiply = 1 #первоначальное значение для последующего умножения
+
+    for i in range(userNumber):
+        commonList.append(randint(-userNumber, userNumber))
+        
+    file = open("file.txt", "r")
+
+    for line in file:
+        print(line)
+        multiply *= commonList[int(line)]
+
+    file.close()
+
+    print(multiply)
+
+
+print()
+userNumber = int(input("Введите число N: "))
+print()
+print("Произведение равняется:", Multiplication(userNumber) )
+
+
+
+
