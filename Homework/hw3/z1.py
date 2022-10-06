11d3ffa5313f1b73efe87ec6af09e7b175e0df23
Anna.K <kkk.aanya1996@mail.ru>
2022-10-06 16:42:51 +0300

    z1 + 1.jpg

--- Homework/hw3/z1.py
+++ Homework/hw3/z1.py
@@ -3,3 +3,24 @@
 Пример:    
 [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 """
+from random import randint
+
+print()
+sizeList = int(input("Задайте размер списка: "))
+initialList  = []
+for i in range(sizeList):
+    initialList.append(randint(1, 10))
+print(initialList)
+print()
+
+def SumOddNums(sizeList, initialList):
+    sum = 0
+    for i in range(sizeList):
+        if i % 2 != 0:
+            sum += int(initialList[i])
+            print("Промежуточная сумма:",sum) # для проверки
+    print()
+    return sum
+
+print("Сумма элементов списка на нечётных позициях:", SumOddNums(sizeList, initialList))
+print()
