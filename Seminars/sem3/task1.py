521ddd7c532121e53868c351991cb4f102a7bebb
Anna.K <kkk.aanya1996@mail.ru>
2022-10-06 21:18:29 +0300

    task1.sem3

--- Seminars/sem3/task1.py
+++ Seminars/sem3/task1.py
@@ -1,2 +1,13 @@
 """ Задача 1. Реализуйте алгоритм задания случайных чисел 
 без использования встроенного генератора псевдослучайных чисел."""
+from mimetypes import read_mime_types
+from time import time
+print()
+print(time())
+print()
+
+def Func(t):
+    return int(str(t).split(".")[1])%100 #функция-разделитель строки
+
+print(Func(time()))
+print()
