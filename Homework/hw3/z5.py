230c30a7bb220f47c519dc6eeb699240f74c5a16
Anna.K <kkk.aanya1996@mail.ru>
2022-10-06 19:38:28 +0300

    z5 + 5.jpg

--- Homework/hw3/z5.py
+++ Homework/hw3/z5.py
@@ -3,3 +3,35 @@
 для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
 [Негафибоначчи](https://ru.wikipedia.org/wiki/Негафибоначчи)
 """
+print()
+userN = int(input("Введите число: "))
+print()
+
+def Fib(userN): 
+    if userN in [1, 2]:
+        return 1
+    else:
+        return Fib(userN-1) + Fib(userN -2)
+
+list = [] #для будущего списка чисел Фибоначчи и Негафибоначчи
+for k in range(1, userN + 1):
+    list.append(Fib(k))
+
+def NegaFib(list):
+    listN = [] #для работы именно с Негафибоначчи
+    for i in range(len(list)): 
+        if (len(list) - i - 1) % 2 != 0:
+            listN.append(-list[-i-1])
+        else:
+            listN.append(list[-i-1])
+    listN.append(0) 
+    for i in range(len(list)): 
+        listN.append(list[i])
+    return listN
+
+print("Список чисел Фибоначчи с положительными и отрицательными индексами:")
+print(NegaFib(list))
+print()
+
+
+
