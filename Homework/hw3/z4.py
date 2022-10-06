24c01a8d52250618235073f520a8b5bb1b076cec
Anna.K <kkk.aanya1996@mail.ru>
2022-10-06 19:17:17 +0300

    z4

--- Homework/hw3/z4.py
+++ Homework/hw3/z4.py
@@ -4,3 +4,22 @@
 3 -> 11
 2 -> 10
 """
+print()
+userN = int(input("Введите число: "))
+print()
+
+def DecimalToBinary (num):
+    binaryList = []
+    while num > 0:
+        remainder = num % 2
+        binaryList.append(remainder)
+
+    for i in range(len(binaryList)//2): 
+        temp = binaryList [i]
+        binaryList[i] = binaryList[len(binaryList) - i - 1]
+        binaryList[len(binaryList) - i - 1] = temp
+
+    return binaryList
+
+print("Десятичное число", userN,"в двоичной системе счисления равно:", DecimalToBinary(userN))
+print()
