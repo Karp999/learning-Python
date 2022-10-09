b72b53887707dc226ae2fe55142c0348b81b81bd
Anna.K <kkk.aanya1996@mail.ru>
2022-10-09 21:15:22 +0300

    z1

--- Homework/hw4/z1.py
+++ Homework/hw4/z1.py
@@ -2,3 +2,12 @@
 Пример:    
 при d = 0.001, π = 3.141    10^(-1) ≤ d ≤10^(-10)
 """
+
+from cmath import pi 
+import numpy
+
+d = numpy.pi
+print("Число Пи = ", d)
+print()
+
+# попробовать через while или for сократить
