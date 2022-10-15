5eb4ba219fb2ae3fdb3ebdd221cb8b17e645bd3b
Anna.K <kkk.aanya1996@mail.ru>
2022-10-16 00:15:11 +0300

    z1 жеребьёвку вписала в ход игры, расписала ход игры. Работаю над обращением к игрокам

--- Homework/hw5/z1.py
+++ Homework/hw5/z1.py
@@ -12,16 +12,55 @@ print()
 print("Приветствуем! Вы играете в игру '2021 конфета'.")
 print("После жеребьёвки каждый из вас по очереди забирает не более 28 конфет.")
 print("Побеждает тот, кто сделает крайний ход.")
-print("Удачи!:)")
 print()
-
-# Жеребьёвка.
-def Draw(): 
-    if randint(0, 1) == 0:
-        Eva = "player1"
-        print("Первый ход достаётся", Eva)
-        return Eva
+# Жеребьёвка и ход игры.
+def GameProgress(firstPlayerCandy, secondPlayerCandy):
+    candy = 2021
+    if randint(0, 1) == 1:
+        first = "Ева"
+        second = "Адам"
+        print(first, ", первый ход ваш.", second, ", вы берёте конфеты после первого игрока .")
     else:
-        Adam = "player2"
-        print("Первый ход достаётся", Adam)
-        return Adam
+        first = "Адам"
+        second = "Ева"
+        print(first, ", первый ход ваш.", second, ", вы берёте конфеты после первого игрока .")
+
+
+    for i in range(candy+1):
+        # Описание ходов Евы.
+        # while take1_candy > candy:
+        #     print("в стопке недостаточно конфет")
+        #     take1_candy = int(input("Первый игрок берет ? конфет: "))
+        moveFirstPlayer = int(input("Ева, сколько вы возьмёте конфет? Введите число от 0 до 28: "))
+        if 0 <= moveFirstPlayer <= 28:
+            firstPlayerCandy = firstPlayerCandy + moveFirstPlayer
+            i = candy - moveFirstPlayer
+            print ("У Евы", firstPlayerCandy, "конфет. В игре осталось", i,"конфет.")
+        if moveFirstPlayer > 28:
+            print("Ева, взять можно от 0 до 28 конфет, повторите ход.")
+        if candy == 0:
+            firstPlayerCandy = firstPlayerCandy + secondPlayerCandy
+            print("Ева, поздравляем, вы победили и все конфеты ваши!:) Игра окончена!")
+            return
+        
+
+        # Описание ходов Адама.
+        moveSecondPlayer = int(input("Адам, сколько вы возьмёте конфет? Введите число от 0 до 28: "))
+        if 0 <= moveSecondPlayer <= 28:
+            secondPlayerCandy = secondPlayerCandy + moveSecondPlayer
+            i = candy - moveSecondPlayer
+            print ("У Адама", secondPlayerCandy, "конфет. В игре осталось", i,"конфет.")
+        if moveSecondPlayer > 28:
+            print("Адам, взять можно от 0 до 28 конфет, повторите ход.")
+        if candy == 0:
+            secondPlayerCandy = firstPlayerCandy + secondPlayerCandy
+            print("Адам, поздравляем, вы победили и все конфеты ваши!:) Игра окончена!")
+            return
+    return
+        
+             
+
+firstPlayerCandy = 0 
+secondPlayerCandy = 0
+print("Игра началась, удачи!:)", GameProgress(firstPlayerCandy, secondPlayerCandy))
+print()
