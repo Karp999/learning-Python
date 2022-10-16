44d1cac944ab111454888fc42b3ac89d081c158b
Anna.K <kkk.aanya1996@mail.ru>
2022-10-16 16:29:10 +0300

    описание методов сжатия и восстановления документов в z3. ошибка FileNotFoundError: [Errno 2] No such file or directory: 'enc.txt', осталась, хотя файл на месте

--- Homework/hw5/z3.py
+++ Homework/hw5/z3.py
@@ -1,6 +1,8 @@
 """ Задача 3 . Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 Входные и выходные данные хранятся в отдельных текстовых файлах. """
 
+#!!! ошибка FileNotFoundError: [Errno 2] No such file or directory: 'enc.txt', хотя файл на месте
+
 print()
 encTXT = "enc.txt"
 decTXT = "dec.txt"
@@ -13,5 +15,44 @@ def ReadingFiles(file):
 
 print(ReadingFiles(encTXT))
 print(ReadingFiles(decTXT))
+
+# Описание сжатия данных
+def Encryption(encTXT):
+    encoding = " "
+    prev_char = " "
+    count = 1
+
+    if not encTXT:
+        return " "
+
+    for char in encTXT:
+        if char != prev_char: # сравниваются текущий и предыдущий символы, при их несовпадении 
+            # посчитанное количество символов и сам символ добавляются в расшифровку
+            if prev_char:
+                encoding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else: 
+            count += 1
+    else:
+        encoding += str(count) + prev_char
+        return encoding
+
+
+# Описание восстановления данных
+def Decoding (decTXT):
+    decode = " "
+    count = " "
+    for char in decTXT:
+        if char.isdigit(): # если символ - дата, присоединяем его к счетчику
+            count += char
+        else: # иначе печатаем количество символов в расшифровку
+            decode += char * int(count)
+            count = " "
+    return decode
+
+Encryption(decTXT)
+Decoding(encTXT)
+
 
 
