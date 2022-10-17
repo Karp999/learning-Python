ece612ee37a4c42b63539a8d61c7b52414b0f74f
Anna.K <kkk.aanya1996@mail.ru>
2022-10-17 19:20:43 +0300

    z3 - корректировала задачу про произведение пар чисел списка

---
+++ Homework/hw6/z3.py
@@ -0,0 +1,40 @@
+""" Задача 2. Напишите программу, которая найдёт произведение пар чисел списка. 
+Парой считаем первый и последний элемент, второй и предпоследний и т.д.    
+Пример:    
+[2, 3, 4, 5, 6] => [12, 15, 16];
+[2, 3, 5, 6] => [12, 15]
+"""
+#ДО:
+# from random import randint
+
+# print()
+# sizeList = int(input("Задайте размер списка: "))
+# initialList  = []
+# for i in range(sizeList):
+#     initialList.append(randint(1, 9))
+# print(initialList)
+# print()
+
+# def Multiplication(initialList):
+#     resultList = []
+#     for i in range(len(initialList)):
+#         if i <= len(initialList)-1-i:
+#             resultList.append(initialList[i]*initialList[len(initialList)-1-i])
+#             print("Промежуточный результат:", resultList) # проверка 
+#     print()
+#     return resultList
+
+# print ("Произведение пар чисел списка:", Multiplication(initialList))
+# print()
+
+#ПОСЛЕ:
+from random import randint
+print()
+sizeList = int(input("Задайте размер списка: "))
+print()
+initialList  = [randint(1, 9) for i in range(sizeList)]
+print(initialList)
+print()
+resultList = [initialList[i]*initialList[len(initialList)-1-i] for i in range(len(initialList)) if i <= len(initialList)-1-i]
+print ("Произведение пар чисел списка:", resultList)
+print()
