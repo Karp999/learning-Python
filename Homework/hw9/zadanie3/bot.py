ef7cd58e3228a18b6a56d417144adada903758f5
Anna.K <kkk.aanya1996@mail.ru>
2022-10-27 19:56:17 +0300

    создала бота для третьей задачи, но он пока не работает

--- Homework/hw9/zadanie3/bot.py
+++ Homework/hw9/zadanie3/bot.py
@@ -1,5 +1,6 @@
 # Прикрутить бота к задачам с предыдущего семинара:
 # b. Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
+# МОЙ БОТ - @A_phonebook_bot
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
